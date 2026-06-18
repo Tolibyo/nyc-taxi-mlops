7e589f3271af8e487e89c8835c3c34c13d395b87
tolibyo <tolibyormahmadov@Tolibs-MacBook-Air.local>
2026-06-18 01:43:18 -0700

    chore(localstack): migrate state to S3 backend with locking

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,24 @@ terraform {
             version = "~> 5.0"
         }
     }
+
+    backend "s3" {
+        bucket = "nyc-taxi-tfstate"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+        skip_credentials_validation = true
+        skip_metadata_api_check = true
+        skip_region_validation = true
+        use_path_style = true
+        skip_requesting_account_id = true
+        endpoints = {
+            s3 = "http://localhost:4566"
+            iam = "http://localhost:4566"
+            sts = "http://localhost:4566"
+        }
+
+        use_lockfile = true
+    }
 }
 
 provider "aws" {
